a0a942fffe926a5044c9bf2da2f14bbd839c420a
Ieremie Ioan <33784294+Ieremie@users.noreply.github.com>
2022-04-20 14:11:28 +0100

    job names could have partition names so 'grep' fails

--- status.sh
+++ status.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-v100=$( squeue | grep 'gpu' |  awk ' $8 !~ /\(/ {print $8}' | sort -u |  wc -l)
+
+v100=$(squeue |  awk ' $2 ~ /\gpu/' |  awk ' $8 !~ /\(/' | awk  '!_[$8]++' | awk '{print $7}' | awk '{s+=$1} END {print s}')
 echo "STATUS v100 GPUS: "$v100"/10 nodes are in use"
 
-gtx=$( squeue | grep 'gtx' |  awk ' $8 !~ /\(/ {print $8}' | sort -u |  wc -l)
+gtx=$(squeue |  awk ' $2 ~ /\gtx1080/' |  awk ' $8 !~ /\(/' | awk  '!_[$8]++' | awk '{print $7}' | awk '{s+=$1} END {print s}')
 echo "STATUS 1080TI GPUS: "$gtx"/10 nodes are in use"
 
-ecsstaff=$(squeue | grep 'ecsstaff' |  awk ' $8 !~ /\(/' | awk '{print $7}' | awk '{s+=$1} END {print s}')
+ecsstaff=$(squeue |  awk ' $2 ~ /\ecsstaff/' |  awk ' $8 !~ /\(/' | awk '{print $7}' | awk '{s+=$1} END {print s}')
 echo "STATUS rtx8000 GPUS: "$ecsstaff"/12 staff GPUS are in use"
 
-ecsstudent=$(squeue | grep 'ecsstu' |  awk ' $8 !~ /\(/' | awk '{print $7}' | awk '{s+=$1} END {print s}')
+ecsstudent=$(squeue |  awk ' $2 ~ /\ecsstudent/' |  awk ' $8 !~ /\(/' | awk '{print $7}' | awk '{s+=$1} END {print s}')
 echo "STATUS rtx8000 GPUS: "$ecsstudent"/8 student GPUS are in use"
+
+
+
+
+
 
